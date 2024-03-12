c561ae0f56c00af95cd7f5c8509c7585f8d081e7
Diego Magno <dio_jp@hotmail.com>
2024-03-11 22:11:15 -0300

    Adicionando a Classe Sport

---
+++ src/main/java/com/kamikase/web/posbackend/controller/EsporteController.java
@@ -0,0 +1,12 @@
+package com.kamikase.web.posbackend.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/esporte")
+public class EsporteController {
+}
